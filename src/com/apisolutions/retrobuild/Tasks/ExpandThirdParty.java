5467c33d5825b0da1d44d43140b2b6c307cc76dc
Boris Farber <bfarber@google.com>
2015-12-30 17:24:56 +0000

    optimised jar extraction

--- src/com/apisolutions/retrobuild/Tasks/ExpandThirdParty.java
+++ src/com/apisolutions/retrobuild/Tasks/ExpandThirdParty.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 public class ExpandThirdParty implements Task {
@@ -27,23 +28,27 @@ public class ExpandThirdParty implements Task {
 
     private static void extractJar(String jarFile, String destDir) throws Exception {
         JarFile jar = new JarFile(jarFile);
-        Enumeration entries = jar.entries();
+        Enumeration<? extends JarEntry> entries = jar.entries();
         while (entries.hasMoreElements()) {
-            java.util.jar.JarEntry file = (java.util.jar.JarEntry) entries.nextElement();
+            JarEntry file = entries.nextElement();
             java.io.File f = new java.io.File(destDir + java.io.File.separator + file.getName());
-            if (file.isDirectory()) { // if its a directory, create it
+            if (file.isDirectory()) {
                 f.mkdir();
                 continue;
             }
-            java.io.InputStream is = jar.getInputStream(file); // get the input stream
 
             f.getParentFile().mkdirs();
             f.createNewFile();
+            java.io.InputStream is = jar.getInputStream(file);
+
             FileOutputStream fos = new FileOutputStream(f);
 
-            while (is.available() > 0) {  // write contents of 'is' to 'fos'
-                fos.write(is.read());
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = is.read(buffer)) != -1) {
+                fos.write(buffer, 0, len);
             }
+
             fos.close();
             is.close();
         }
@@ -52,7 +57,6 @@ public class ExpandThirdParty implements Task {
     private static void collectFileNamesTo(ArrayList<File> to, String from) {
         File directory = new File(from);
 
-        // get all the files from a directory
         File[] fList = directory.listFiles();
         for (File file : fList) {
             if (file.isFile()) {
